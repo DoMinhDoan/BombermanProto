19dba3e7ac4ad4a8f72fe5769a839f5a9b961a4b
doan.dominh <doan.dominh@gameloft.com>
2018-12-19 14:30:47 +0700

    - bomb explosion in 4 axis

--- Assets/Scripts/Bomb.cs
+++ Assets/Scripts/Bomb.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     public GameObject explosionPrefab;
+    public LayerMask layerMask;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +23,12 @@ public class Bomb : MonoBehaviour
     {
         Instantiate(explosionPrefab, transform.position, Quaternion.identity);
 
+        //
+        StartCoroutine(CreateExplosion(Vector3.forward));
+        StartCoroutine(CreateExplosion(Vector3.right));
+        StartCoroutine(CreateExplosion(Vector3.back));
+        StartCoroutine(CreateExplosion(Vector3.left));
+
         // Disables the mesh renderer, making the bomb invisible.
         GetComponent<MeshRenderer>().enabled = false;
 
@@ -29,5 +37,27 @@ public class Bomb : MonoBehaviour
 
         // Destroys the bomb after 0.3 seconds; this ensures all explosions will spawn before the GameObject is destroyed.
         Destroy(gameObject);
+    }
+
+    IEnumerator CreateExplosion(Vector3 direction)
+    {
+        for (int i = 1; i < 3; i++)
+        {
+            RaycastHit hit;
+
+            // Raycast : position, direction, maxdistance, layerMask
+            Physics.Raycast(transform.position + new Vector3(0, 0.5f, 0), direction, out hit, i, layerMask);
+
+            if(!hit.collider)
+            {
+                Instantiate(explosionPrefab, transform.position + (i * direction), explosionPrefab.transform.rotation);
+            }
+            else
+            {
+                // Once the raycast hits a block, it breaks out of the for loop.This ensures the explosion can't jump over walls.
+                break;
+            }
+        }
+        yield return new WaitForSeconds(.05f);
     }
 }
